e8f563cc9a72c57f7f43c623ccd32cec3bc2a7e0
AbrahamOtero <abraham.otero@gmail.com>
2022-04-19 11:51:01 +0200

    Capítulo de introducción
    Primera versión del capítulo 1

---
+++ 1_introduction/1.1ElEntorno.R
@@ -0,0 +1,49 @@
+# Todo lo que comienza por el símbolo "#" es un comentario
+
+#Sumamos dos números; prueba a teclear esto en la consola (Ventana "Console").
+#Prueba también a poner el cursor en esta línea y pulsar CTRL+Enter
+#Por último, prueba también a poner el cursor en la línea y pulsar el botón de "Run"
+3+4
+
+# Los operadores de suma, resta, multiplicación y división son respectivamente
+#+, -, * y / . Ejecuta las siguientes sentencias:
+
+2+3
+4-5
+3*5
+2/3
+
+#Las variables permiten almacenar datos que pueden cambiar en el futuro.
+#Podemos pensar en ella como una "caja" donde podemos meter un dato, el "contenido
+#de la caja". Creemos una variable con nombre "x" y con valor 2
+
+x = 2 #Observa cómo ha aparecido la variable en el "Global Environment" a la derecha
+x
+
+# Una variable puede cambiar de valor
+
+x = 5
+x
+y = 2*x
+y
+
+#Ojo que se distingue entre mayúsculas y minúsculas;la siguiente sentencia da un error
+
+z = 3*X + y
+
+#La sintaxis correcta es
+
+z = 3*x + y
+
+# Además del operador "=" puede emplearse el operador "<-" para las asignaciones
+
+var <- x/y + 5*z
+number <- 5 
+
+# Los números reales siempre deben ponerse con "."
+
+real = 4.5
+
+#Podemos ver todos los objetos del entorno con el comando objects
+
+objects()
