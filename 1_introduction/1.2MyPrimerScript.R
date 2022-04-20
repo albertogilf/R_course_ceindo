51a27ee29efe576df3a65ee4d1294591f5232387
AbrahamOtero <abraham.otero@gmail.com>
2022-04-20 10:03:21 +0200

    Pequeñas correcciones para el capítulo 1

--- 1_introduction/1.2MyPrimerScript.R
+++ 1_introduction/1.2MyPrimerScript.R
@@ -1,18 +1,18 @@
-# Un Script Es un conjunto de instrucciones  pensadas para ejecutarse  conjuntamente
+# Un Script es un conjunto de instrucciones  pensadas para ejecutarse  conjuntamente
 #y así conseguir resolver un determinado problema. Este script calcula las soluciones
-#para ana ecuación de segundo grado (ax^2+bx+c=0). Los valores a, b y c se especifican
+#para una ecuación de segundo grado (ax^2+bx+c=0). Los valores a, b y c se especifican
 #a continuación:
 
 a=1
 b=2
 c=-3
 
-discr= sqrt(b*b -4*a*c)
+discr = sqrt(b*b -4*a*c)
 raiz1 = (-b+discr)/(2*a)
 raiz2 = (-b-discr)/(2*a)
 
 #print() Muestra un mensaje por pantalla; paste() permite crear un único texto 
-#concatenando variables y texto
+#concatenando variables y texto que se separan por ","
 print(paste("Las raíces son", raiz1, "y", raiz2))
 
 #Prueba a ejecutar todo el script con el botón "Source" (parte superior derecha del editor)
